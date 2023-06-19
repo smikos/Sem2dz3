9ca32c8294b9269606a4d2b54c4352005767f6d6
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-19 20:00:06 +0300

    work good

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿int Prompt(string message)
+{
+    System.Console.Write( message );
+    int value= Convert.ToInt32(Console.ReadLine());
+    return value ;
+}
+int num = Prompt("chislo > ");
+if ( 5<num && num <8 )
+{
+    Console.WriteLine ("yes");
+
+}
+else Console.WriteLine("no");
